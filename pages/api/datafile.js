19fdd2494b0eb3303edb078691d96dc37991885c
Rudra Narayan Boitei <rudra2021@gift.edu.in>
2023-01-04 11:13:51 +0530

    fixed some issue

--- pages/api/datafile.js
+++ pages/api/datafile.js
@@ -1,12 +1,8 @@
-// import path from 'path';
-// import { promises as fs } from 'fs';
+import * as fs from "fs";
 
-// export default async function handler(req, res) {
-//   //Find the absolute path of the json directory
-//   const jsonDirectory = path.join(process.cwd(), 'json/userData.json');
-//   //Read the json data file data.json
-//   const fileContents = await fs.readFile(jsonDirectory , 'utf8');
-// //   console.log(fileContents);
-//   //Return the content of the data file in json format
-//   res.status(200).json(fileContents);
-// }
+export default function handler(req,res) {
+    fs.readFile(`../../userData.json`,'utf-8', (error,data) => {
+        console.log(req.query.slug);
+        res.status(200).json(JSON.parse(data) )
+    })
+}
